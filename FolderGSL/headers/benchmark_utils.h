01543c34b042ff1309e53f5ab8fe2f1a6c97737a
Christian <chris_fish5@web.de>
2017-05-10 12:57:28 +0200

    Commented benchmark_utils.h

--- FolderGSL/headers/benchmark_utils.h
+++ FolderGSL/headers/benchmark_utils.h
@@ -1,11 +1,23 @@
 #ifndef BENCHMARK_UTILS_H_INCLUDED
 #define BENCHMARK_UTILS_H_INCLUDED
 
-void initCsv(const char* filename, unsigned num_devices);
-void writeToCsv(const char* filename, unsigned V, unsigned E, unsigned device_id, unsigned long time);
+
+// Function to check if to arrays of size length and type cl_uint are identical, i.e. arr1[i][j] equals arr2[i][j] for i,j > 0
 bool cl_uint_arr_equal(cl_uint* arr1, cl_uint* arr2, unsigned length);
+
+// Function to check if to arrays of size length and type cl_float are identical, i.e. arr1[i] equals arr2[i] for all i >= 0
 bool cl_float_arr_equal(cl_float* arr1, cl_float* arr2, unsigned length);
-void generate_path_name(const char* filename, char* pathname);
+
+// Function to generate a pathname for the csv files
+void generate_path_name_csv(const char* filename, char* pathname);
+
+// Creates Csv File if not existing and generates the columns, one for the graph and num_devices for the devices
+void initCsv(const char* filename, unsigned num_devices);
+
+// Function to write results of benchmarking in the csv File, V/E denotes the number of vertices/edges of the graph, device_id the device and time the actual time it took
+void writeToCsv(const char* filename, unsigned V, unsigned E, unsigned device_id, unsigned long time);
+
+// Function that returns the time in milliseconds, can be used for time measuring
 unsigned long time_ms();
 
 
