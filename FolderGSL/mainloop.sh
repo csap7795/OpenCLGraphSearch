9cd4a25e2bd02fcd4f497d74ad03ecd3fd430130
Christian <chris_fish5@web.de>
2017-06-01 13:16:56 +0200

    Fixed

--- FolderGSL/mainloop.sh
+++ FolderGSL/mainloop.sh
@@ -4,6 +4,6 @@ export LD_LIBRARY_PATH="$PWD/bin/Debug"
 _files=Graph/*.g
 for f in $_files;
 do
-	_filename=$(basename $_files)
+	#_filename=$(basename $_files)
 	./myprog "$f" #> logs/"$_filename.txt"
 done
